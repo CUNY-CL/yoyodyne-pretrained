8f40edad4928a1ea891e5facc0cb763c5cc1739d
Kyle Gorman <kylebgorman@gmail.com>
2025-09-26 12:39:59 -0400

    Last-minute cleanups

--- yoyodyne_pretrained/cli.py
+++ yoyodyne_pretrained/cli.py
@@ -28,6 +28,11 @@ class YoyodynePretrainedCLI(cli.LightningCLI):
             required=False,
         )
         parser.link_arguments("model.init_args.model_name", "data.model_name")
+        parser.link_arguments(
+            "data.model_dir",
+            "trainer.logger.init_args.save_dir",
+            apply_on="instantiate",
+        )
 
 
 def main() -> None:
@@ -40,6 +45,7 @@ def main() -> None:
     YoyodynePretrainedCLI(
         model_class=models.BaseModel,
         datamodule_class=data.DataModule,
+        save_config_callback=None,
         subclass_mode_model=True,
         trainer_class=trainers.Trainer,
     )
@@ -50,6 +56,7 @@ def python_interface(args: cli.ArgsType = None) -> None:
     YoyodynePretrainedCLI(
         models.BaseModel,
         data.DataModule,
+        save_config_callback=None,
         subclass_mode_model=True,
         trainer_class=trainers.Trainer,
         args=args,
