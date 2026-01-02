7f235d3b713c7d3afe51560397397b42bff7caec
Kyle Gorman <kylebgorman@gmail.com>
2026-01-02 10:41:11 -0500

    merge

--- yoyodyne_pretrained/data/datasets.py
+++ yoyodyne_pretrained/data/datasets.py
@@ -33,7 +33,7 @@ class IterableDataset(AbstractDataset, data.IterableDataset):
 
 
 @dataclasses.dataclass
-class MappableDataset(data.Dataset):
+class MappableDataset(AbstractDataset):
     """Mappable (random access) dataset.
 
     This is implemented with a memory map after making a single pass through
